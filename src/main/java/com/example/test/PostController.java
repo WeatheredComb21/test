b27871a753034b6c2b50c712812011d27a52b41e
WeatheredComb21 <mateusz.bala02@gmail.com>
2021-02-24 19:33:23 +0100

    first commit

---
+++ src/main/java/com/example/test/PostController.java
@@ -0,0 +1,16 @@
+package com.example.test;
+import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+    @GetMapping("/hello")
+  public String Hello() {
+    return "Hello world";
+  }
+  @GetMapping("/addPost")
+  public Post[] AddPost(String title, String content) throws IOException {
+    return JsonActions.addPost(title,content);
+  }
+}
